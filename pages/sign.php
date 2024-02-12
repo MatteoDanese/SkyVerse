24fd0c7a4ae08f74852cf62154cab48003b486ef
MatteoDanese <6190140@itisrossi.vi.it>
2024-02-12 09:05:03 +0100

    fix

--- pages/sign.php
+++ pages/sign.php
@@ -1,37 +1,37 @@
 <?php
-$user = $_POST['username'];
-$pass = $_POST['password'];
-$email = $_POST['email'];
-$present = false;
+    $user = $_POST['username'];
+    $pass = $_POST['password'];
+    $email = $_POST['email'];
+    $present = false;
 
-require_once('../DataBase/DB.php');
+    require_once('../DataBase/DB.php');
 
-$DB = new DB();
+    $DB = new DB();
 
-$sql= "INSERT INTO utenti (username, email, password) VALUES (?, ?, ?)";
-$params = [$user, $email, $pass];
-$types = "sss";
-$result = $DB->preparedQuery($sql, $params, $types);
+    $sql= "INSERT INTO utenti (username, email, password) VALUES (?, ?, ?)";
+    $params = [$user, $email, $pass];
+    $types = "sss";
+    $result = $DB->preparedQuery($sql, $params, $types);
 
-if ($result->num_rows > 0) {
-$row = $result->fetch_assoc();
-setcookie('username', $row['username'], time() + 86400, '/');
-header("Location: ../index.php");
-} else {
-    echo "Login fallito. Verifica le credenziali.";
-}
-#------------------------BACKUP SU FILE JSON-------------------------
-$utenti = json_decode(file_get_contents('utenti.json'), true);
-$utente = [
-    'username' => $user,
-    'email' => $email,
-    'password' => $pass
-];
-$utenti[] = $utente;
-file_put_contents('utenti.json', json_encode($utenti, JSON_PRETTY_PRINT));
-#------------------------/BACKUP SU FILE JSON/-------------------------
+    if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    setcookie('username', $row['username'], time() + 86400, '/');
+    header("Location: ../index.php");
+    } else {
+        echo "Login fallito. Verifica le credenziali.";
+    }
+    #------------------------BACKUP SU FILE JSON-------------------------
+    $utenti = json_decode(file_get_contents('utenti.json'), true);
+    $utente = [
+        'username' => $user,
+        'email' => $email,
+        'password' => $pass
+    ];
+    $utenti[] = $utente;
+    file_put_contents('utenti.json', json_encode($utenti, JSON_PRETTY_PRINT));
+    #------------------------/BACKUP SU FILE JSON/-------------------------
 
-header("Location: login.php");
+    header("Location: login.php");
 
 
 ?>
