439d8b0b2553e4940766dcd1962386919252f700
MatteoDanese <6190140@itisrossi.vi.it>
2024-02-15 11:43:52 +0100

    hashing password

--- pages/sign.php
+++ pages/sign.php
@@ -1,10 +1,9 @@
 <?php
-    $user = $_POST['username'];
-    $pass = $_POST['password'];
-    $email = $_POST['email'];
-    $present = false;
-
     require_once('../DataBase/DB.php');
+
+    $user = $_POST['username'];
+    $email = $_POST['email'];
+    $pass = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
     $DB = new DB();
 
@@ -14,12 +13,13 @@
     $result = $DB->preparedQuery($sql, $params, $types);
 
     if ($result->num_rows > 0) {
-    $row = $result->fetch_assoc();
-    setcookie('username', $row['username'], time() + 86400, '/');
-    header("Location: ../index.php");
+        $row = $result->fetch_assoc();
+        setcookie('username', $row['username'], time() + 86400, '/');
+        header("Location: ../index.php");
     } else {
         echo "Login fallito. Verifica le credenziali.";
     }
+
     #------------------------BACKUP SU FILE JSON-------------------------
     $utenti = json_decode(file_get_contents('utenti.json'), true);
     $utente = [
@@ -29,9 +29,7 @@
     ];
     $utenti[] = $utente;
     file_put_contents('utenti.json', json_encode($utenti, JSON_PRETTY_PRINT));
+
     #------------------------/BACKUP SU FILE JSON/-------------------------
-
     header("Location: login.php");
-
-
 ?>
--- pages/verify.php
+++ pages/verify.php
@@ -4,20 +4,25 @@
     $user = $_POST['username'];
     $email = $_POST['email'];
     $pass = $_POST['password'];
-    $present = false;
 
     $DB = new DB();
 
-    $sql= "SELECT * FROM utenti WHERE username=? AND email=? AND password=?";
-    $params = [$user, $email, $pass];
-    $types = "sss";
+    $sql= "SELECT * FROM utenti WHERE username=? AND email=?";
+    $params = [$user, $email];
+    $types = "ss";
     $result = $DB->preparedQuery($sql, $params, $types);
 
     if ($result->num_rows > 0) {
-    $row = $result->fetch_assoc();
-    setcookie('username', $row['username'], time() + 86400, '/');
-    header("Location: ../index.php");
+        $row = $result->fetch_assoc();
+
+        if (password_verify($pass, $row['password'])) {
+            setcookie('username', $row['username'], time() + 86400, '/');
+            header("Location: ../index.php");
+            exit();
+        } else {
+            echo "Login fallito, verifica la password";
+        }
     } else {
-        echo "Login fallito. Verifica le credenziali.";
+        echo "Login fallito, l'utente non è stato trovato.";
     }
 ?>
